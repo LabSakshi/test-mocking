112b9712a02bd912a2a16175fe8791f388265fe8
Sakshi Gupta <sakshigupta0513@gmail.com>
2022-02-20 18:20:47 +0530

    this contains removal of element from DOM

--- sundae/src/pages/summary/test/SummaryForm.test.jsx
+++ sundae/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,9 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+//GET BY: WHEN THE ELEMENT IS IN THE DOM/////
+// QUERY BY: WHEN THE ELEMENT IS NOT IN THE DOM// 
+//FIND BY: WHEN ELEMENT WILL BE THERE BUT ONLY AFTER THE API REQUEST IS SUCCESSFULL
+
+
+import { render, fireEvent, screen , waitForElementToBeRemoved} from "@testing-library/react";
 import SummaryForm from '../SummaryForm'
 import userEvent from '@testing-library/user-event'
 
@@ -21,10 +26,19 @@ test('Checking the Terms and condition checkbox enables the confirm order button
     expect(confirmButton).toBeDisabled()
 })
 
-test('popover responds to hover',()=>{
+test('popover responds to hover', () => {
+    render(<SummaryForm />)
     //popover starts out hidden
-
+    const nullPopover = screen.queryByText(/no icecream will actually be delivered/i)
+    expect(nullPopover).not.toBeInTheDocument()
     //popover appears upon mouseover of checkbox label
-    
+    const termsAndConditions = screen.getByText(/terms and conditions/i)
+    userEvent.hover(termsAndConditions)
+    const popover = screen.getByText(/no icecream will actually be delivered/i)
+    expect(popover).toBeInTheDocument() //not need this assertion as get by throws the error when elemtn is not there in the dom
     //popover disappears when we mouseout
+    userEvent.click(termsAndConditions)
+    //{waitForElementToBeRemoved} this is occuring asynchronously [that is it occurs after the test is completed] so we will await this
+    expect(screen.queryByText(/no icecream will actually be delivered/i)).toBeNull();
+  
 })
